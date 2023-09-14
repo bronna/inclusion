52fe8a101ab94faff39f8a6f77dd8560d7e99802
Brianna Wilson <bronna@gmail.com>
2023-09-13 17:59:39 -0700

    colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,18 +1,20 @@
 export const colors = [
     //'rgba(67,182,161,1.0)',  // #43B6A1 teal
-    'rgba(73, 171, 160, 1.0)', // #43A9B6 teal 2
-    'rgba(255,181,39,1.0)',  // #FFB527 yellow
+    'rgba(70, 181, 166, 1.0)', // #43A9B6 teal 2
+    //'rgba(255,181,39,1.0)',  // #FFB527 yellow
+    'rgba(248, 170, 22, 1.0)', // #F8AA16 yellow 2'
     //'rgba(245,115,70,1.0)',  // #F57346 orange
-    'rgba(245, 129, 61, 1.0)', // #F5813D orange 2
+    'rgba(247, 134, 68, 1.0)', // #F5813D orange 2
     //'rgba(207,66,120,1.0)',  // #CF4278 dark pink
-    'rgba(213, 68, 92, 1.0)',  // #DB4162 dark pink 2
+    'rgba(224, 84, 84, 1.0)',  // #DB4162 dark pink 2
     //'rgba(67,182,161,0.6)',  // teal reduced opacity
-    'rgba(73, 171, 160, 0.6)', // teal 2 reduced opacity
-    'rgba(255,181,39,0.7',  // yellow reduced opacity
+    'rgba(70, 181, 166, 0.6)', // teal 2 reduced opacity
+    //'rgba(255,181,39,0.7',  // yellow reduced opacity
+    'rgba(248, 170, 22, 0.7)', // yellow 2 reduced opacity'
     //'rgba(245,115,70,0.6)',  // orange reduced opacity
-    'rgba(245, 129, 61, 0.6)', // orange 2 reduced opacity
+    'rgba(247, 134, 68, 0.6)', // orange 2 reduced opacity
     //'rgba(207,66,120,0.5)',  // dark pink reduced opacity
-    'rgba(213, 68, 92, 0.5)',  // dark pink 2 reduced opacity
+    'rgba(224, 84, 84, 0.5)',  // dark pink 2 reduced opacity
     '#777677', // dark gray
     '#001A17', // text dark
     '#e6e6e6', // light gray
