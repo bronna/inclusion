a02494c82b7bd0e8acd8dcc485119fa6e409b015
Brianna Wilson <bronna@gmail.com>
2024-01-30 13:26:50 -0800

    updated definitions

--- src/components/Sources.svelte
+++ src/components/Sources.svelte
@@ -1,11 +1,11 @@
 <hr class="text-width" />
 
 <div class="data-source text-width">
+    <p>The <strong>inclusion score</strong> is calculated based on reported rates of how much time students with disabilities spend in regular classrooms.</p>
+
+    <p>"<strong>Small districts</strong>" are defined as having less than 500 students with IEPs. "<strong>Large districts</strong>" have 500 or more.</p>
+
     <p>Data is from the 2021-22 school year (unless otherwise noted) and comes from the <a href="https://www.ode.state.or.us/data/reportcard/media.aspx" target="_blank">Oregon Department of Education</a>. Geography data comes from the <a href="https://www.census.gov/cgi-bin/geo/shapefiles/index.php?year=2022&layergroup=School+Districts" target="_blank">US Census</a></p>
-
-    <p>The inclusion score is calculated based on reported rates of how much time students with disabilities spend in regular classrooms.</p>
-
-    <p>"Large districts" are defined as having 500 or more students with IEPs.</p>
 
     <p><a href="https://github.com/bronna/inclusion/tree/main/src/data" target="_blank">Methodology</a></p>
 </div>
