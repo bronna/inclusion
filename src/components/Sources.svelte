cbb650ec18f16e208c7745ee06d5f23107fe0a55
Brianna Wilson <bronna@gmail.com>
2024-01-30 13:20:40 -0800

    updated definitions

--- src/components/Sources.svelte
+++ src/components/Sources.svelte
@@ -3,7 +3,7 @@
 <div class="data-source text-width">
     <p>Data is from the 2021-22 school year (unless otherwise noted) and comes from the <a href="https://www.ode.state.or.us/data/reportcard/media.aspx" target="_blank">Oregon Department of Education</a>. Geography data comes from the <a href="https://www.census.gov/cgi-bin/geo/shapefiles/index.php?year=2022&layergroup=School+Districts" target="_blank">US Census</a></p>
 
-    <p>The inclusion score is calculated based on the reported rates of how much of the day, on average, a student with disabilities spends in a regular classroom.</p>
+    <p>The inclusion score is calculated based on reported rates of how much time students with disabilities spend in regular classrooms.</p>
 
     <p>"Large districts" are defined as having 500 or more students with IEPs.</p>
 
